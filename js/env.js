6fb2505564164e442aed5c533eed366fe5447399
SaraLPZ <79020122+SaraLPZ@users.noreply.github.com>
2022-08-03 21:32:50 +0200

    Add files via upload

---
+++ js/env.js
@@ -0,0 +1,3 @@
+
+const APP_ID = ''  /* String App id from Agora */
+export { APP_ID }
---
+++ js/join.js
@@ -0,0 +1,12 @@
+let lobbyForm = document.getElementById('lobby__form')
+lobbyForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    sessionStorage.setItem('display_name', e.target.name.value)
+    window.location = `index.html?room=${e.target.room.value}`
+})
+
+lobbyForm.room.addEventListener('keyup', (e) => {
+    let cleaned_value = e.target.value.replace(' ', '')
+    e.target.value = cleaned_value.toUpperCase()
+})
